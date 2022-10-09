ce27e58b221e5f180fbea16a3bdcbce52b537cea
Marc M <gazmarcou@outlook.fr>
2022-10-09 23:50:10 +0200

    style: modified navbar (select page is now selected)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,11 @@
-<!DOCTYPE html>
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+$routes = ["dashboard"];
+?>
+
+    <!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -63,23 +70,25 @@
         </div>
         <div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="navbar-cta">
             <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-800 dark:border-gray-700">
-                <li>
-                    <a href="#"
-                       class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-                       aria-current="page">Dashboard</a>
-                </li>
-                <li>
-                    <a href="#"
-                       class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-                </li>
-                <li>
-                    <a href="#"
-                       class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-                </li>
-                <li>
-                    <a href="#"
-                       class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-                </li>
+
+                @foreach($routes as $str)
+                    @if($str === Route::currentRouteName())
+
+                        <li>
+                            <a href=""
+                               class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                               aria-current="page">{{ ucfirst($str) }}</a>
+                        </li>
+
+                    @else
+
+                        <li>
+                            <a href="{{ route($str) }}"
+                               class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{{ ucfirst($str) }}</a>
+                        </li>
+
+                    @endif
+                @endforeach
             </ul>
         </div>
     </div>
